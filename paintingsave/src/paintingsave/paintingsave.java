960098bdbfe3a9a2137514729ad5a20083b20d8e
scr41255 <reinhold.schlager@st.oth-regensburg.de>
2025-10-01 00:36:36 +0200

    fixed haning painting.

--- paintingsave/src/paintingsave/paintingsave.java
+++ paintingsave/src/paintingsave/paintingsave.java
@@ -1,32 +1,20 @@
 package paintingsave;
-import net.canarymod.plugin.Plugin;
-import net.canarymod.logger.Logman;
 import net.canarymod.Canary;
-import net.canarymod.commandsys.*;
-import net.canarymod.chat.MessageReceiver;
-import net.canarymod.api.entity.living.humanoid.Player;
 import com.pragprog.ahmine.ez.EZPlugin;
 import net.canarymod.plugin.PluginListener;
 import net.canarymod.hook.HookHandler;
 import net.canarymod.hook.entity.HangingEntityDestroyHook;
-import net.canarymod.api.entity.Entity;
-import net.canarymod.api.entity.EntityType;
 
 public class paintingsave extends EZPlugin implements PluginListener {
 
-@Override 
+  @Override 
   public boolean enable() {
+    Canary.hooks().registerListener(this, this);
+    return super.enable();
+  }
 
-  Canary.hooks().registerListener(this, this);
-  return super.enable();
-
-                          }
-
-@HookHandler
-public void dasbleibthaengen(HangingEntityDestroyHook event){
-
-  Entity abhaengen = event.getPainting();
-  event.setCanceled();
-
- }
+  @HookHandler
+  public void HangingEntityDestroyHookEvent(HangingEntityDestroyHook event){
+    event.setCanceled();
+  }
 }
