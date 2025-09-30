002740b3daee05ae8bc46e29a517aac29aa60cf0
scr41255 <reinhold.schlager@st.oth-regensburg.de>
2025-09-30 16:54:41 +0200

    Refactored formating and removed unused imports.

--- AlwaysSun/src/alwayssun/AlwaysSun.java
+++ AlwaysSun/src/alwayssun/AlwaysSun.java
@@ -1,10 +1,5 @@
 package alwayssun;
-import net.canarymod.plugin.Plugin;
-import net.canarymod.logger.Logman;
 import net.canarymod.Canary;
-import net.canarymod.commandsys.*;
-import net.canarymod.chat.MessageReceiver;
-import net.canarymod.api.entity.living.humanoid.Player;
 import com.pragprog.ahmine.ez.EZPlugin;
 import net.canarymod.hook.HookHandler;
 import net.canarymod.plugin.PluginListener;
@@ -14,29 +9,20 @@ import net.canarymod.api.world.World;
 
 public class AlwaysSun extends EZPlugin implements PluginListener{
   
-  @Override 
-   public boolean enable() {
+	@Override 
+	public boolean enable() {
+   		Canary.hooks().registerListener(this, this);
+   		return super.enable();
+	}
 
-   Canary.hooks().registerListener(this, this);
-   return super.enable();
- }
-
- @HookHandler
- public void dasbleibthaengen(WeatherChangeHook event){
-  	event.setCanceled();
- }
-
- @HookHandler 
- public void noTimeChange(TimeChangeHook event){
- 	World world = event.getWorld();
- 	event.setCanceled();
- 	/*
- 	if(world.getRelativeTime()!=1000){
- 		logger.info("es ist nicht so weit!");
- 		world.setTime(1000);
+ 	@HookHandler
+	public void WeatherChangeHookMethod(WeatherChangeHook event){
+  		event.setCanceled();
  	}
- 	*/
- }
 
-
+ 	@HookHandler 
+ 	public void noTimeChange(TimeChangeHook event){
+ 		World world = event.getWorld();
+ 		event.setCanceled();
+ 	}
 }
